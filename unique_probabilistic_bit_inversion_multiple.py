1363ae8917cdcf62c9ab19f3445258bd3c4b08f7
fran <franciscomartiarena@gmail.com>
2026-01-16 11:51:18 -0300

    vid

---
+++ unique_probabilistic_bit_inversion_multiple.py
@@ -0,0 +1,20 @@
+import numpy as np
+from PIL import Image
+
+p = 0.2
+img = Image.open("image3.png").convert("RGB")
+
+for i in np.arange(100):  # probabilidad de invertir el MSB
+
+    
+    arr = np.array(img, dtype=np.uint8)
+
+    # Máscara Bernoulli(p)
+    mask = (np.random.rand(*arr.shape) < p).astype(np.uint8)
+
+    # Invertir MSB solo donde mask == 1
+    arr_prob = arr ^ (mask * 128)
+
+    img_out = Image.fromarray(arr_prob, mode="RGB")
+    img_out.save(f"bitswap_frames/{i}.png")
+    print(f"guardada imagen {i}")
